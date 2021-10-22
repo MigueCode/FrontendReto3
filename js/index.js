cab4c9c7030fe32faafc89cbb2bfc5ebb6c87ddc
MigueCode <bastidasluis1188@gmail.com>
2021-10-21 22:09:13 -0500

    Index, estilos de reseteo y botones en Js

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// Variable del Document
+const $d = document
+
+// Variables de los botones Consultar 
+const $consultarCategorias = $d.getElementById("category-get"),
+         $consultarBarcos = $d.getElementById("boat-get"),
+         $consultarClientes = $d.getElementById("client-get"),
+         $consultarMensajes = $d.getElementById("message-get"),
+         $consultarReservaciones = $d.getElementById("reservation-get")
+
+// Variables de los botones Guardar
+const $guardarCategorias = $d.getElementById("category-save"),
+         $guardarBarcos = $d.getElementById("boat-save"),
+         $guardarClientes = $d.getElementById("client-save"),
+         $guardarMensajes = $d.getElementById("message-save"),
+         $guardarReservaciones = $d.getElementById("reservation-save")
+
